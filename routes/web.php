5c877dc0b7ad3fb53451c6bd1a6e6c7669817ed8
BayuFirmansyaah <bayufirmansyah2800@gmail.com>
2025-05-10 16:09:28 +0700

    (add) - xss

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     return to_route('login');
@@ -22,3 +23,33 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => 'au
 });
 
 
+Route::get('/write/session', function () {
+    $payload = request()->query('payload');
+
+    if (is_null($payload)) {
+        return response("Parameter 'payload' tidak ditemukan.", 400);
+    }
+
+    $content = "Payload: " . $payload . "\n";
+    
+    $path = public_path('session.txt');
+
+    File::put($path, $content);
+
+    return "Payload berhasil ditulis ke session.txt";
+});
+
+Route::get('/session', function(){
+    $path = public_path('session.txt');
+
+    // Cek apakah file ada
+    if (!File::exists($path)) {
+        return response("File session.txt tidak ditemukan.", 404);
+    }
+
+    // Ambil isi file
+    $content = File::get($path);
+
+    // Tampilkan ke browser (bisa juga gunakan nl2br untuk format HTML)
+    return response("<pre>$content</pre>");
+});
